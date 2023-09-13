ed1c87d494ca05e2fe57319b0c784e2410a3d3c0
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-09-13 16:22:24 +0700

    Isolate move into function

--- Final/Final.ino
+++ Final/Final.ino
@@ -1,9 +1,46 @@
 #include "src/Motor.h"
 
+#define REVERSE 0
+#define FORWARD 1
+
+#define SPEED 20
 #define LEFT_WHEEL 1
 #define RIGHT_WHEEL 2
 #define DirectionPin 4
 #define BaudRate 115200
+
+void move(float distance_cm, int mode) {
+
+    float distance_per_sec = 7;
+    int target_time = round(distance_cm * (1000 / distance_per_sec));
+
+    switch (mode) {
+
+        case FORWARD:
+
+            Motor.turnWheel(LEFT_WHEEL, LEFT, SPEED);
+            Motor.turnWheel(RIGHT_WHEEL, RIGHT, SPEED);
+
+            delay(target_time);
+
+            break;
+        
+        case REVERSE:
+            
+            Motor.turnWheel(LEFT_WHEEL, RIGHT, SPEED);
+            Motor.turnWheel(RIGHT_WHEEL, LEFT, SPEED);
+
+            delay(target_time);
+
+            break;
+        
+        default:
+            break;
+
+    }
+
+}
+
 
 void setup() {
 
@@ -12,63 +49,60 @@ void setup() {
     Motor.begin(BaudRate, DirectionPin, &Serial2);
 
     // To preparation point
-    Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
-
-    delay(8570);
+    move(60, FORWARD);
     
     // Turn to target #1
-    Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, LEFT, 20);
+    move(12.5, REVERSE);
 
-    delay(2143);
-
+    // Rotate left 90deg
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
 
-    delay(1950);
+    delay(1857);
 
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 0);
     Motor.turnWheel(RIGHT_WHEEL, LEFT, 0);
     delay(100);
-
     
-    Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, LEFT, 20);
-
-    delay(8751);
+    move(61.25, REVERSE);
 
     // Back to preparation point
-    Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
+    move(61.25, FORWARD);
 
-    delay(8214);
-
+    // Rotate right 90deg
     Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
     Motor.turnWheel(RIGHT_WHEEL, LEFT, 20);
 
     delay(1950);
 
-    Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
+    move(12.5, FORWARD);
 
-    delay(1786);
+    // Stop for a while
+    Motor.turnWheel(LEFT_WHEEL, LEFT, 0);
+    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
+
+    delay(500);
+
+    move(25, FORWARD);
 
     Motor.turnWheel(LEFT_WHEEL, LEFT, 0);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
 
     delay(500);
 
+    // Turn to beeper
     Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
+    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
 
-    delay(4857);
+    delay(1000);
+
+    Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
+    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
+
+    delay(1000);
 
     // Back to preparation point
-    Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, LEFT, 20);
-
-    delay(3929);
+    move(22.5, REVERSE);
 
     // Turn to target #3
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
@@ -76,10 +110,7 @@ void setup() {
 
     delay(1950);
 
-    Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
-
-    delay(6429);
+    move(45, FORWARD);
 
     Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
     Motor.turnWheel(RIGHT_WHEEL, LEFT, 20);
@@ -88,30 +119,24 @@ void setup() {
 
     // Rotate around half-circle
     Motor.turnWheel(LEFT_WHEEL, LEFT, 35);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 70);
-    delay(4200);
+    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 75);
+    delay(3600);
+
+    move(15, FORWARD);
 
     // Last turn
-    Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
-
-    delay(500);
-    
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
 
-    delay(900);
+    delay(1100);
 
     // Last forward
-    Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
-    Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
-
-    delay(7143);
+    move(45, FORWARD);
 
     Motor.turnWheel(LEFT_WHEEL, LEFT, 0);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
     
-
 }
+
 
 void loop() {}
