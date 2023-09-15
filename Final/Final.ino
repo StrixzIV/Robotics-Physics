af5272a3063590daa1d2fdff25486d32bed4e343
Jirayu Kaewsing <jirayukaewsing48@gmail.com>
2023-09-15 13:21:54 +0700

    Renaming variables and delete whitespaces

--- Final/Final.ino
+++ Final/Final.ino
@@ -6,14 +6,16 @@
 #define SPEED 20
 #define LEFT_WHEEL 1
 #define RIGHT_WHEEL 2
-#define DirectionPin 4
-#define BaudRate 115200
+#define DIRECTION_PIN 4
+#define BRAUD_RATE 115200
+
+// a distance that robot could move in 1 second.
+const float DISTANCE_PER_SEC = 7;
 
 // Move robot for `d` cm in `mode` direction
 void move(float distance_cm, int mode) {
 
-    float distance_per_sec = 7;
-    int target_time = round(distance_cm * (1000 / distance_per_sec));
+    int time_to_target = round(distance_cm * (1000 / DISTANCE_PER_SEC));
 
     switch (mode) {
 
@@ -22,7 +24,7 @@ void move(float distance_cm, int mode) {
             Motor.turnWheel(LEFT_WHEEL, LEFT, SPEED);
             Motor.turnWheel(RIGHT_WHEEL, RIGHT, SPEED);
 
-            delay(target_time);
+            delay(time_to_target);
 
             break;
         
@@ -31,7 +33,7 @@ void move(float distance_cm, int mode) {
             Motor.turnWheel(LEFT_WHEEL, RIGHT, SPEED);
             Motor.turnWheel(RIGHT_WHEEL, LEFT, SPEED);
 
-            delay(target_time);
+            delay(time_to_target);
 
             break;
         
@@ -54,23 +56,21 @@ void pause(int time_ms) {
 void setup() {
 
     // Initialization
-    Serial.begin(BaudRate);
-    Motor.begin(BaudRate, DirectionPin, &Serial2);
+    Serial.begin(BRAUD_RATE);
+    Motor.begin(BRAUD_RATE, DIRECTION_PIN, &Serial2);
 
     // To preparation point
     move(60, FORWARD);
     
-    // Turn to target #1
+    // Move to target #1
     move(12.5, REVERSE);
 
     // Rotate left 90deg
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
-
     delay(1857);
 
     pause(100);
-    
     move(61.25, REVERSE);
 
     // Back to preparation point
@@ -79,43 +79,36 @@ void setup() {
     // Rotate right 90deg
     Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
     Motor.turnWheel(RIGHT_WHEEL, LEFT, 20);
-
     delay(1950);
 
     move(12.5, FORWARD);
-
-    // Stop for a while
     pause(500);
 
+    // Move to target #2
     move(25, FORWARD);
-
     pause(500);
 
     // Turn to beeper
     Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
-
     delay(1000);
 
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
-
     delay(1000);
 
     // Back to preparation point
     move(22.5, REVERSE);
 
-    // Turn to target #3
+    // Move to target #3
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
-
     delay(1950);
 
     move(45, FORWARD);
 
     Motor.turnWheel(LEFT_WHEEL, LEFT, 20);
     Motor.turnWheel(RIGHT_WHEEL, LEFT, 20);
-
     delay(1950);
 
     // Rotate around half-circle
@@ -123,17 +116,18 @@ void setup() {
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 75);
     delay(3600);
 
+    // Move forward to last turning point
     move(15, FORWARD);
 
     // Last turn
     Motor.turnWheel(LEFT_WHEEL, RIGHT, 20);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 20);
-
     delay(1100);
 
     // Last forward
     move(45, FORWARD);
 
+    // Stop the robot
     Motor.turnWheel(LEFT_WHEEL, LEFT, 0);
     Motor.turnWheel(RIGHT_WHEEL, RIGHT, 0);
     
